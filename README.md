9c13471f0c3b1c938177dfda45481cb097a37eff
JANSONkevin <kevin.janson.pro@gmail.com>
2021-05-30 18:56:08 +0200

    quete 12

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ https://www.youtube.com/watch?v=F6ZnpAzy_TI
 lien vers la video de la quete 11:
 https://www.youtube.com/watch?v=xXCNfh4q3_8
 
+
+lien vers la video de la quete 12:
+https://www.youtube.com/watch?v=LGsMiOkdy_g
+
+
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/programs", name="program_")
@@ -28,6 +30,25 @@ class ProgramController extends AbstractController
             'programs' => $programs,
         ]);
     }
+
+     /**
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        $program = new Program();
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($program);
+            $entityManager->flush();
+            return $this->redirectToRoute('program_index');
+        }
+        // Render the form
+        return $this->render('program/new.html.twig', ["form" => $form->createView()]);
+    }
+
 
     /**
      * @Route ("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
